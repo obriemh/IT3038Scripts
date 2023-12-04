ad14a3a394412832f9dbdf62757bd5bbb32cdf41
obriemh <143364062+obriemh@users.noreply.github.com>
2023-12-03 22:55:19 -0500

    Add files via upload

---
+++ python/Final Project/CFBScoreScraper.py
@@ -0,0 +1,18 @@
+from bs4 import BeautifulSoup
+import requests
+import pandas as pd
+
+headers = {'User-Agent': 'Mozilla/5.0'}
+
+url = r"https://www.sports-reference.com/cfb/schools/cincinnati/2023-schedule.html"
+source = requests.get(url, headers= headers)
+soup = BeautifulSoup(source.content, 'lxml')
+
+header = [th.getText() for th in soup.findAll('tr', limit=2)[0].findAll('th')]
+header = header[1:]
+rows = soup.findAll('tr')[1:]
+
+game_stats = [[td.getText() for td in rows[i].findAll('td')] for i in range(len(rows))]
+stats = pd.DataFrame(game_stats, columns=header)
+print(stats)
+
